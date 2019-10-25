56ec1b11cd14a34316f681b88a06cc4d4ab38c17
James <james689@users.noreply.github.com>
2019-10-25 17:27:28 +0100

    complete basic game flow

--- HangmanGUI.java
+++ HangmanGUI.java
@@ -13,12 +13,12 @@ import javax.swing.Box;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Hangman extends JPanel {
+public class HangmanGUI extends JPanel {
 	
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Hangman");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Hangman content = new Hangman();
+		HangmanGUI content = new HangmanGUI();
 		window.setContentPane(content);
 		window.pack();
 		window.setVisible(true);
@@ -26,55 +26,69 @@ public class Hangman extends JPanel {
 	
 	// --------------------------------------
 	
-	private JPanel imagePanel; // displays the hangman image on screen
-	private JLabel wordLabel; // displays the word user is trying to guess
+	private JLabel wordLabel; // displays the word the player is trying to guess
 	private JTextField textInputBox; 
 	private JButton newGameButton;
 	private String message = "welcome to hangman";
 	private HangmanGame game;
 	
-	public Hangman() {
-		game = new HangmanGame(6);
-		//setPreferredSize(new Dimension(500,500));
+	public HangmanGUI() {
+		game = new HangmanGame(6); // start the player in a new game when GUI is created
+		
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		setBackground(Color.GREEN);
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		imagePanel = new ImagePanel();
-		add(imagePanel);
+		
 		wordLabel = new JLabel(game.getGuessString());
 		wordLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		
+		textInputBox = new JTextField(5);
+		textInputBox.addActionListener(new TextInputBoxListener());
+		
+		newGameButton = new JButton("New Game");
+		newGameButton.addActionListener(new NewGameButtonListener());
+		newGameButton.setEnabled(false);
+		
+		add(new ImagePanel());
 		add(wordLabel);
 		add(Box.createRigidArea(new Dimension(0,10)));
 		add(new JLabel("Guess letter (press enter to submit)"));
-		textInputBox = new JTextField(5);
-		textInputBox.addActionListener(new InputListener());
 		add(textInputBox);
 		add(Box.createRigidArea(new Dimension(0,10)));
-		newGameButton = new JButton("new game");
-		newGameButton.addActionListener(new NewGameButtonListener());
-		newGameButton.setEnabled(false);
 		add(newGameButton);
 	}
 	
-	private class InputListener implements ActionListener {
-		// called when user presses enter key in text input box
+	// listener for the textInputBox
+	private class TextInputBoxListener implements ActionListener {
+		// called when user presses enter key in the text input box
 		public void actionPerformed(ActionEvent e) {
+			// don't let the player make guesses if the game is over. This should
+			// not be able to occur anyway because the textInputBox is disabled when
+			// the game is over but just in case.
 			if (game.isGameOver()) {
 				message = "game is over. Start a new game to play again";
 				repaint();
 				return;
 			}
-			String userInput = textInputBox.getText();
-			textInputBox.setText("");
-			// check to make sure text is valid
 			
-			//System.out.println("user pressed enter");
-			game.makeGuess('d');
+			// get the player's guess
+			String playerGuess = textInputBox.getText();
+			textInputBox.setText("");
+			// check to make sure text is valid e.g. only one char entered, not a digit etc
+			if (game.hasGuessedAlready(playerGuess.charAt(0))) {
+				message = "you have already guessed this letter";
+				repaint();
+				return;
+			}
+			
+			game.makeGuess(playerGuess.charAt(0));
+			wordLabel.setText(game.getGuessString());
+			message = "";
 			if (game.isGameOver()) {
 				if (game.hasWon()) {
 					message = "congratulations, you win!";
 				} else {
-					message = "sorry, you lose. The word was: " + game.getWordToGuess() + "\n press new game to play again";
+					message = "sorry, you lose. The word was: " + game.getWordToGuess();
 				}
 				textInputBox.setEnabled(false);
 				newGameButton.setEnabled(true);
@@ -83,8 +97,13 @@ public class Hangman extends JPanel {
 		}
 	}
 	
+	// listener for the newGameButton
 	private class NewGameButtonListener implements ActionListener {
+		// called when user clicks on the new game button
 		public void actionPerformed(ActionEvent e) {
+			// don't let the player start a new game if there is already a game in progress.
+			// This should not be able to occur anyway because the newGameButton is disabled when
+			// the game is in progress but just in case.
 			if (!game.isGameOver()) {
 				message = "you still have to finish this game first";
 				repaint();
@@ -95,9 +114,13 @@ public class Hangman extends JPanel {
 			newGameButton.setEnabled(false);
 			textInputBox.setEnabled(true);
 			textInputBox.setText("");
+			message = "";
+			repaint();
 		}
 	}
 	
+	// this nested panel draws the hangman image and message
+	// to the screen.
 	private class ImagePanel extends JPanel {
 		
 		public ImagePanel() {
@@ -111,7 +134,7 @@ public class Hangman extends JPanel {
 			super.paintComponent(g);
 			
 			// draw gallows
-			int numIncorrectGuesses = game.getNumIncorrectGuesses();
+			int numIncorrectGuesses = game.getIncorrectGuessesMade();
 			if (numIncorrectGuesses >= 1) {
 				// draw head
 			}
@@ -130,6 +153,8 @@ public class Hangman extends JPanel {
 			if (numIncorrectGuesses == 6) {
 				// draw right leg
 			}
+			
+			g.drawString("" + numIncorrectGuesses, 20, 100);
 			
 			// draw message at bottom of screen
 			g.drawString(message,20,200);
--- HangmanGame.java
+++ HangmanGame.java
@@ -4,18 +4,21 @@ import java.util.Set;
 // This class represents the hangman game logic
 public class HangmanGame {
 	
-	private String wordToGuess; // the word the player is trying to guess
-	private int numIncorrectGuesses; // number of incorrect guesses player has made
-	private int numIncorrectGuessesAllowed; // number of incorrect guesses player is allowed to make before its game over
-	private Set<Character> guessesMade; // all guesses made so far
+	private String wordToGuess; 			// the word the player has to guess
+	private int incorrectGuessesMade; 		// number of incorrect guesses player has made
+	private int incorrectGuessesAllowed; 	// number of incorrect guesses player is allowed to make before its game over. This
+											// can be set in the constructor, to make the game easier or more difficult.
+	private Set<Character> guessesMade; 	// all guesses made so far
 	
-	public HangmanGame(int numIncorrectGuessesAllowed) {
-		this.numIncorrectGuessesAllowed = numIncorrectGuessesAllowed;
-		wordToGuess = "hippopotemous"; // in reality random word will be picked from dictionary here
-		numIncorrectGuesses = 0;
+	public HangmanGame(int incorrectGuessesAllowed) {
+		this.incorrectGuessesAllowed = incorrectGuessesAllowed;
+		wordToGuess = "hippo"; // in reality random word will be picked from dictionary here
+		incorrectGuessesMade = 0;
 		guessesMade = new HashSet<>();
 	}
 	
+	// if each letter in wordToGuess is contained within the guessesMade set
+	// the player has won.
 	public boolean hasWon() {
 		for (int i = 0; i < wordToGuess.length(); i++) {
 			char c = wordToGuess.charAt(i);
@@ -26,11 +29,11 @@ public class HangmanGame {
 		return true;
 	}
 	
-	public int getNumIncorrectGuesses() {
-		return numIncorrectGuesses;
+	public int getIncorrectGuessesMade() {
+		return incorrectGuessesMade;
 	}
 	
-	public boolean hasAlreadyGuessedCharacter(char c) {
+	public boolean hasGuessedAlready(char c) {
 		return guessesMade.contains(c);
 	}
 	
@@ -38,12 +41,18 @@ public class HangmanGame {
 		return wordToGuess;
 	}
 	
-	public void makeGuess(char c) {
-		/*if (isGameOver()) {
+	public void makeGuess(char letter) {
+		// don't allow the guess if the game is over or
+		// the letter has already been guessed. The client class
+		// should prevent this from happening anyway but just in case.
+		if (isGameOver() || hasGuessedAlready(letter)) {
 			return;
 		}
-		guessesMade.add(c);*/
-		numIncorrectGuesses++;
+		
+		guessesMade.add(letter);
+		if (wordToGuess.indexOf(letter) == -1) { // letter is not in wordToGuess
+			incorrectGuessesMade++;
+		}
 	}
 	
 	public String getGuessString() {
@@ -58,6 +67,6 @@ public class HangmanGame {
 	}
 	
 	public boolean isGameOver() {
-		return hasWon() || (numIncorrectGuesses == numIncorrectGuessesAllowed);
+		return hasWon() || (incorrectGuessesMade == incorrectGuessesAllowed);
 	}
 }
